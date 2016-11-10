863b28e0a0108f5e72d63fbee777b0398c974836
changfangyuan <1332236790@qq.com>
2016-11-10 09:32:37 +0800

    second

---
+++ 1811/js/goods.js
@@ -0,0 +1,12 @@
+$(function(){
+//购物车;
+	$(".goods_top").click(function(){
+		$('body,html').animate({scrollTop:0},30);
+			return false;
+	});
+	$(".sideNav_list").find(".top_end").click(function(){
+		$('body,html').animate({scrollTop:0},30);
+			return false;
+	});
+})
+
